f7a4a2d0669950a17671bd77762cafa77f680da6
kristianakila <kristianakila@ya.ru>
2025-11-30 13:30:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,37 @@ import express from "express";
 import cors from "cors";
 import tinkoffRouter from "./routes/tinkoff.js";
 
+import admin from "firebase-admin";
+
+// ====== Firebase Admin через Render ENV ======
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      type: process.env.FIREBASE_TYPE,
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_AUTH_URI,
+      token_uri: process.env.FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
+      client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
+    }),
+  });
+}
+
+export const db = admin.firestore();
+
+// ====== Express ======
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Роуты
 app.use("/api", tinkoffRouter);
 
+// Порт
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
