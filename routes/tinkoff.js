ca4c3aa1c75eb1c034f9cf7b387374df72c8f191
kristianakila <kristianakila@ya.ru>
2025-12-01 15:10:16 +0300

    Update tinkoff.js

--- routes/tinkoff.js
+++ routes/tinkoff.js
@@ -119,7 +119,7 @@ router.post("/init", async (req, res) => {
       Token: token,
       Description: description,
       CustomerKey: userId,
-      Recurrent: recurrent, // Добавлен параметр рекуррента (после CustomerKey, перед PayType)
+      Recurrent: Y, // Добавлен параметр рекуррента (после CustomerKey, перед PayType)
       // PayType: "O", // Можно добавить при необходимости
       // Language: "ru", // Можно добавить при необходимости
       Receipt: {
